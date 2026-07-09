0f79de4cef70276b2f017a33a2637fd484d0dd3a
suvamgurung <gurungsuvam265@gmail.com>
2026-07-09 14:15:13 +0545

    Update: Added authorized http matchers, login and logout

--- spring-token-practice/src/main/java/com/suvam/springtokenpractice/config/OfficerConfig.java
+++ spring-token-practice/src/main/java/com/suvam/springtokenpractice/config/OfficerConfig.java
@@ -1,4 +1,34 @@
 package com.suvam.springtokenpractice.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.stereotype.Component;
+
+@Component
 public class OfficerConfig {
+
+    @Bean
+    public SecurityFilterChain addSecurityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(
+                                "/officer",
+                                "/get-token",
+                                "/"
+                        )
+                        .permitAll()
+                        .anyRequest()
+                        .authenticated()
+                )
+                .formLogin(login -> login
+                        .defaultSuccessUrl("/officer", true)
+                        .permitAll()
+                )
+                .logout(log -> log
+                        .logoutSuccessUrl("/logout?logout")
+                        .permitAll()
+                );
+        return http.build();
+    }
 }
