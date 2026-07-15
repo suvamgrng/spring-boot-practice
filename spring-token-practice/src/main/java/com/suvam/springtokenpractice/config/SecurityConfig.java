89ff8407174deaf20cc1d3c36e17cf1e1380c0c3
suvamgurung <gurungsuvam265@gmail.com>
2026-07-15 23:45:02 +0545

    Update: Password protection with hashed

--- spring-token-practice/src/main/java/com/suvam/springtokenpractice/config/SecurityConfig.java
+++ spring-token-practice/src/main/java/com/suvam/springtokenpractice/config/SecurityConfig.java
@@ -71,6 +71,6 @@ public class SecurityConfig {
 
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return NoOpPasswordEncoder.getInstance();
+        return new BCryptPasswordEncoder();
     }
 }
