eb86afdf674cc2db531cb928051d121972fbb2bd
suvamgurung <gurungsuvam265@gmail.com>
2026-07-17 09:47:48 +0545

    Add: Configuration and Component to create custom filtered chain

--- Teacher/src/main/java/com/suvam/teacher/configuration/SecurityConfig.java
+++ Teacher/src/main/java/com/suvam/teacher/configuration/SecurityConfig.java
@@ -1,4 +1,10 @@
 package com.suvam.teacher.configuration;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+
+@Configuration
+@Component
 public class SecurityConfig {
+
 }
