3fc67e014c41bda95e54c1c5ca71c8f930e75ac5
suvamgurung <gurungsuvam265@gmail.com>
2026-07-19 05:02:09 +0545

    Add: Connecting authentication with db

--- Teacher/src/main/java/com/suvam/teacher/configuration/SecurityConfig.java
+++ Teacher/src/main/java/com/suvam/teacher/configuration/SecurityConfig.java
@@ -2,12 +2,16 @@ package com.suvam.teacher.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 @Configuration
@@ -37,8 +41,20 @@ public class SecurityConfig {
         UserDetails user = User.builder()
                 .username("amisha")
                 .password("{noop}amisha123")
-                .roles("asdf")
+                .roles("user")
                 .build();
         return new InMemoryUserDetailsManager(user);
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider(UserDetailsService userDetailsService) {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider(userDetailsService);
+        provider.setPasswordEncoder(passwordEncoder());
+        return provider;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
     }
 }
