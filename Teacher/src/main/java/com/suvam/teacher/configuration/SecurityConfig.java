cf60c1654dda1206c99a7efa3253cdddb3962943
suvamgurung <gurungsuvam265@gmail.com>
2026-07-18 16:03:59 +0545

    Add: Custom UserDetailsService and added login details

--- Teacher/src/main/java/com/suvam/teacher/configuration/SecurityConfig.java
+++ Teacher/src/main/java/com/suvam/teacher/configuration/SecurityConfig.java
@@ -4,6 +4,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 @EnableWebSecurity
@@ -28,5 +32,15 @@ public class SecurityConfig {
                         .logoutSuccessUrl("/login")
                         .permitAll());
         return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetails() {
+        UserDetails user = User.builder()
+                .username("amisha")
+                .password("amisha123")
+                .roles("asdf")
+                .build();
+        return new InMemoryUserDetailsManager(user);
     }
 }
