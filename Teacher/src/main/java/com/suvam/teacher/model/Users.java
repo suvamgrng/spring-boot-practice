5768a586528bd08dba8a5f240fb0e5764ff09af4
suvamgurung <gurungsuvam265@gmail.com>
2026-07-19 15:26:38 +0545

    Update: Several fields added

--- Teacher/src/main/java/com/suvam/teacher/model/Users.java
+++ Teacher/src/main/java/com/suvam/teacher/model/Users.java
@@ -1,4 +1,23 @@
 package com.suvam.teacher.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@AllArgsConstructor
+@Data
+@NoArgsConstructor
 public class Users {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Integer id;
+    String username;
+    String password;
+    String role;
 }
