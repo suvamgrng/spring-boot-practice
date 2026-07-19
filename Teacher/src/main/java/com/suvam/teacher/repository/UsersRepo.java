e2634b3fd5c37ac38948838e928e9d133c6e3ec6
suvamgurung <gurungsuvam265@gmail.com>
2026-07-19 16:18:24 +0545

    Fix: Added annotation to create a bean

--- Teacher/src/main/java/com/suvam/teacher/repository/UsersRepo.java
+++ Teacher/src/main/java/com/suvam/teacher/repository/UsersRepo.java
@@ -3,7 +3,9 @@ package com.suvam.teacher.repository;
 import com.suvam.teacher.model.Teacher;
 import com.suvam.teacher.model.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UsersRepo extends JpaRepository<Teacher, Integer> {
     Users findByUsername(String username);
 }
