0994ae1f1685903a11c449fa17104d3db7aa8155
suvamgurung <gurungsuvam265@gmail.com>
2026-08-02 13:03:19 +0545

    Add: custom hardcode jwt token

--- spring-token-practice/src/main/java/com/suvam/springtokenpractice/service/JwtService.java
+++ spring-token-practice/src/main/java/com/suvam/springtokenpractice/service/JwtService.java
@@ -5,4 +5,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class JwtService {
 
+    public String generateToken() {
+        return "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IlN1dmFtIEd1cnVuZyIsImFkbWluIjp0cnVlLCJpYXQiOjE1MTYyMzkwMjJ9.SKJv4Mmuj2zZhXApi7uScWsvANh6l6QgaucUt2H9GkM";
+    }
 }
