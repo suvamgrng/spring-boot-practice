1964c628023d626db329aa8dc57e74291987e22c
suvamgurung <gurungsuvam265@gmail.com>
2026-08-08 20:04:45 +0545

    Fix: @Repository in UsersRepo

--- Teacher/src/main/java/com/suvam/teacher/repository/UsersRepo.java
+++ Teacher/src/main/java/com/suvam/teacher/repository/UsersRepo.java
@@ -2,9 +2,11 @@ package com.suvam.teacher.repository;
 
 import com.suvam.teacher.model.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface UsersRepo extends JpaRepository<Users, Integer> {
     Optional<Users> findByUsername(String username);
 }
